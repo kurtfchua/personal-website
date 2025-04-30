9f2138d8384aa72f4ae5c8d40d4c8aa14915c821
kurt chua <kurtfchua@gmail.com>
2025-04-29 20:39:33 -0500

    added hero animation w Framer Motion v12

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,23 @@
+'use client'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type HeroProps = {
   name: string
   title: string
 }
-
+const MotionDiv = motion('div')
 const Hero: React.FC<HeroProps> = ({ name, title }) => {
   return (
-    <section className="text-center py-20 px-4 bg-white text-black">
+    <MotionDiv
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2.75 }}
+      className="text-center py-20 px-4 bg-white text-black"
+    >
       <h1 className="text-4xl font-bold mb-2">Hi, I'm {name}</h1>
       <p className="text-xl text-gray-700">{title}</p>
-    </section>
+    </MotionDiv>
   )
 }
 
